1d59235a912126cca69b3b2b26809162b25b3f23
istiyakmi9 <istiyaq.mi9@gmail.com>
2023-04-30 11:09:20 +0530

    local con

--- src/main/java/com/bot/performance/PerformanceApplication.java
+++ src/main/java/com/bot/performance/PerformanceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @EnableDiscoveryClient
 public class PerformanceApplication {
 
